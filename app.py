15f5e43b29a166bee59063bf974c6c6c4aa6e785
NikoKoskinen <47375@edu.raseko.fi>
2024-09-24 12:45:36 +0300

    tähän astiset moduulit

---
+++ app.py
@@ -0,0 +1,21 @@
+# SOVELLUKSEN PÄÄOHJELMA
+# ======================
+
+# KIRJASTOT 
+# ---------------------
+# MODUULIT
+#-------------------------
+import sound # Äänimoduuli
+import video # video moduuli
+
+# ASETUKSET
+# ---------
+kameraIndeksi = 1 # Ensimmäinen kamera on aina 0
+
+# Käynnistetään viedokuva ja ilmoitetaan sen käynnistymisestä äänimerkillä
+sound.parametricBeep(425,500)
+
+# TESTIT KOODAUKSEN AIKANA
+#----------------------------
+if __name__ == "__main__":
+    pass
